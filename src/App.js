3a57d3d124ec2a98c26b29c278b0296662f8c713
jkim8 <juno0218@naver.com>
2022-10-27 10:45:38 -0400

    button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
+  const isError = true;
+
   return (
     <View style={styles.container}>
-      <Text>RN Calc</Text>
+      <Text style={styles.text}>RN Calc</Text>
+      <Text style={[styles.text, isError && styles.error]}>RN Calc2</Text>
+      <Button
+        title="button"
+        color={'green'}
+        onPress={() => console.log('click')}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +23,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 80,
+    color: 'blue',
+  },
+  error: {
+    fontSize: 40,
+    color: 'red',
   },
 });
