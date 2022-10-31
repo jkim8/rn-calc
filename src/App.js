973a8794633f25f36beb7eab35a1563686345dac
jkim8 <juno0218@naver.com>
2022-10-31 17:04:52 -0400

    button making

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 import Button, { ButtonTypes } from './components/Button';
 
 export default function App() {
   const [result, setResult] = useState(0);
+  const width = (useWindowDimensions().width - 5) / 4;
+  // const { width } = useWindowDimensions();
+  console.log(width);
 
   return (
     <View style={styles.container}>
@@ -14,7 +17,28 @@ export default function App() {
       </View>
       <View style={styles.buttonContainer}>
         <View style={styles.leftPad}>
-          <View style={styles.number}></View>
+          <View style={styles.number}>
+            <Button
+              title="1"
+              onPress={() => {}}
+              buttonStyle={{ width, height: width, marginBottom: 1 }}
+            ></Button>
+            <Button
+              title="2"
+              onPress={() => {}}
+              buttonStyle={{ width, height: width, marginBottom: 1 }}
+            ></Button>
+            <Button
+              title="3"
+              onPress={() => {}}
+              buttonStyle={{ width, height: width, marginBottom: 1 }}
+            ></Button>
+            <Button
+              title="4"
+              onPress={() => {}}
+              buttonStyle={{ width, height: width, marginBottom: 1 }}
+            ></Button>
+          </View>
           <View style={styles.bottom}></View>
         </View>
 
@@ -40,7 +64,6 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     backgroundColor: 'skyblue',
-    flex: 1,
     flexDirection: 'row',
   },
   result: { color: '#ffffff', fontSize: 60, fontWeight: '700', padding: 30 },
